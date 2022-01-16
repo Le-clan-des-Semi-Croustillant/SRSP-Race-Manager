bd04241f003d2357bc52a367f09a90e95080d03b
Sky <fabien-perot@orange.fr>
2022-01-16 16:01:30 +0100

    JsonParse

---
+++ Race Manager/DataProssesing/JsonParse.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+
+namespace Race_Manager.DataProssesing
+{
+    public class JsonParse
+    {
+        public static dynamic GameIdJsonDeserialize(string data)
+        {
+            return JsonConvert.DeserializeObject(data);
+        }
+        public static string DataFormatting(IMessageType TypeMessage, long Id, long IdGame, string NMEA, long Boat)
+        {
+            var serialiseJsonInfo = new JsonInformation
+            {
+                TypeMessage = TypeMessage,
+                Id = Id,
+                IdGame = IdGame,
+                NMEA = NMEA,
+                Boat = Boat
+            };
+            return System.Text.Json.JsonSerializer.Serialize(serialiseJsonInfo);
+        }
+    }
+
+    internal class GestionJson
+    {
+        public static void TraitementIMessageType()
+        {
+            JsonParse.GameIdJsonDeserialize(data);
+
+        }
+    }
+
+    public enum IMessageType
+    {
+        CONNECTION,
+        DISCONNECTION,
+        INFO,
+        BOATSELECT,
+    }
+
+    class JsonInformation
+    {
+        public IMessageType TypeMessage { get; set; }
+        public long Id { get; set; }
+        public long IdGame { get; set; }
+        public string? NMEA { get; set; }
+        public long Boat { get; set; }
+
+    }
+
+    class EnvironmentInfo
+    {
+        public int IdPlayer { get; set; }
+
+    }
+}
