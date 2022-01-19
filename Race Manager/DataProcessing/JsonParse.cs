adae5d6d2a2e504de8d34049cd0bdc368e95456a
Sky <fabien-perot@orange.fr>
2022-01-19 22:29:18 +0100

    Update JsonParse
    All function (not completed) for parse json receive

--- Race Manager/DataProcessing/JsonParse.cs
+++ Race Manager/DataProcessing/JsonParse.cs
@@ -9,49 +9,83 @@ namespace Race_Manager.DataProcessing
         {
             return JsonConvert.DeserializeObject(data);
         }
-        public static string JsonSerialise(IMessageType TypeMessage, long Id, long IdGame, string NMEA, long Boat)
+        public static string JsonSerialiseConnection(IMessageType TypeMessage, long Id, long IdGame, string NMEA, long Boat, int IdPlayer, string NamePlayer)
         {
-            var serialiseJsonInfo = new JsonInformation
+            var serialiseJsonInfo = new JsonInformationConnection
             {
                 TypeMessage = TypeMessage,
                 Id = Id,
                 IdGame = IdGame,
                 NMEA = NMEA,
-                Boat = Boat
+                Boat = Boat,
+                EnvironmentInfos = new EnvironmentInfo
+                {
+                    IdPlayer = IdPlayer,
+                    NamePlayer = NamePlayer
+                }
+
+            };
+            return System.Text.Json.JsonSerializer.Serialize(serialiseJsonInfo);
+        }
+
+        public static string JsonSerialiseDisconnection(IMessageType TypeMessage)
+        {
+            var serialiseJsonInfo = new JsonInformationDisconnection
+            {
+                TypeMessage = TypeMessage,
+
+            };
+            return System.Text.Json.JsonSerializer.Serialize(serialiseJsonInfo);
+        }
+
+        public static string JsonSerialiseInfo(IMessageType TypeMessage)
+        {
+            var serialiseJsonInfo = new JsonInformationInfo
+            {
+                TypeMessage = TypeMessage,
+
+            };
+            return System.Text.Json.JsonSerializer.Serialize(serialiseJsonInfo);
+        }
+        public static string JsonSerialiseBoatSelect(IMessageType TypeMessage)
+        {
+            var serialiseJsonInfo = new JsonInformationBoatSelect
+            {
+                TypeMessage = TypeMessage,
+
             };
             return System.Text.Json.JsonSerializer.Serialize(serialiseJsonInfo);
         }
     }
 
-    internal class GestionJson
-    {
-        public static void TraitementIMessageType()
-        {
-            //JsonParse.JsonDeserialize(data);
-
-        }
-    }
-
-    //public enum IMessageType
-    //{
-    //    CONNECTION,
-    //    DISCONNECTION,
-    //    INFO,
-    //    BOATSELECT,
-    //}
-
-    class JsonInformation
+    class JsonInformationConnection
     {
         public IMessageType TypeMessage { get; set; }
         public long Id { get; set; }
         public long IdGame { get; set; }
         public string? NMEA { get; set; }
         public long Boat { get; set; }
+        public EnvironmentInfo? EnvironmentInfos { get; set; }
+    }
+
+    class JsonInformationDisconnection
+    {
+        public IMessageType TypeMessage { get; set; }
+    }
+
+    class JsonInformationInfo
+    {
+        public IMessageType TypeMessage { get; set; }
+    }
+
+    class JsonInformationBoatSelect
+    {
+        public IMessageType TypeMessage { get; set; }
     }
 
     class EnvironmentInfo
     {
         public int IdPlayer { get; set; }
-
+        public string? NamePlayer { get; set; }
     }
 }
