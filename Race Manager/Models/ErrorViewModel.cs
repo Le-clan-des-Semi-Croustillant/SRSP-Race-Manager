9e69951b500fddccd77fc2697ce3822a708ef123
Jérémie Rodez <jeremierodez@outlook.com>
2022-01-20 14:17:55 +0100

    No more MVC Asp.net

--- Race Manager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Race_Manager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
