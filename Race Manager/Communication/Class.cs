6173945a9926ccfa95732f965fbe6d7f76abb173
wolfy-Crag <dark.hunter100@hotmail.com>
2022-01-20 12:32:42 +0100

    Vérification du port
    Rajout d'une portion de code, qui permet lorsqu'on lui donne un port de vérifier si le port en question est disponible.

---
+++ Race Manager/Communication/Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+
+namespace Race_Manager.Communication
+{
+    public class Port
+    {
+        void Portdispo(int port)
+        {
+            bool portDisponible = true;
+
+            IPGlobalProperties ipGlobalProperties = IPGlobalProperties.GetIPGlobalProperties();
+            TcpConnectionInformation[] tcpConnInfoArray = ipGlobalProperties.GetActiveTcpConnections();
+            foreach (TcpConnectionInformation tcpi in tcpConnInfoArray)
+            {
+
+                if (tcpi.LocalEndPoint.Port == port)
+                {
+                    portDisponible = false;
+                    Console.WriteLine("Le port numero {0} est non disponible", port);
+                    break;
+                }
+
+            }
+            Console.WriteLine("Le port numero {0} est disponible", port);
+
+        }
+    }
+}
+
+
+
+
