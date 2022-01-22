cba357358857a483db924e55d9acaa8d148f27b1
wolfy-Crag <dark.hunter100@hotmail.com>
2022-01-23 00:58:52 +0100

    push pour debug error
    debug error

---
+++ Race Manager/Views/Home/Maconfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace Race_Manager.Views.Home
+{
+    public class Maconfig
+    {
+        public static void Main(string[] args)
+        {
+            // Récupérer le nom de l'hôte
+            string host = Dns.GetHostName();
+            Console.WriteLine("Le nom de l'hôte est :" + host);
+            // Récupérer l'adresse IP
+            string ip = Dns.GetHostByName(host).AddressList[0].ToString();
+            Console.WriteLine("Mon adresse IP est :" + ip);
+        }
+    }
+}
+
+
+
+  
