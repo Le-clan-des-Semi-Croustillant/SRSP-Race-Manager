e833ce8bf2ddebde5e8960944fca4abfd6ad53a9
wolfy-Crag <dark.hunter100@hotmail.com>
2022-01-30 12:40:57 +0100

    rajoute code lecture gpx
    balise wpt ne se lis pas correctement

---
+++ Race Manager/Lecture/XMLReadandParse.cs
@@ -0,0 +1,48 @@
+﻿namespace RaceManager.Lecture
+{
+    using System;
+    using System.Xml;
+
+    namespace XMLReadAndParse
+    {
+        class XMLReadandParse
+        {
+            static void Main(string[] args)
+            {
+
+                using (XmlReader reader = XmlReader.Create("D:\\Point de départs.gpx"))
+                {
+                    int numeroWaypoint = 1;
+
+                    while (reader.Read())
+                    {
+
+                        if (reader.IsStartElement())
+                        {
+
+
+                            switch (reader.Name.ToString())
+                            {
+
+                                case "name":
+
+                                    Console.WriteLine("Voici le waypoint numero " + numeroWaypoint + " rencontrer et voisin son nom : " + reader.ReadString() + "\n");
+                                    numeroWaypoint += 1;
+
+                                    break;
+                                case "wpt":
+
+                                    Console.WriteLine("Ma longitude et latitude est " + reader.ReadString());
+                                    break;
+                            }
+                        }
+
+
+
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
