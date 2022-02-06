eadd0ea0e820a8b60a64c6544c357eac64ad0e4e
wolfy-Crag <dark.hunter100@hotmail.com>
2022-02-07 00:53:49 +0100

    Update XMLReadandParse.cs
    Lecture fichier caracteristiqueDeJauge+ typeBateauAutorise +gpf + creationDeLaClasse Englobante

--- Race Manager/Lecture/XMLReadandParse.cs
+++ Race Manager/Lecture/XMLReadandParse.cs
@@ -5,15 +5,13 @@
     using System.Linq;
     using System.Xml.Linq;
     using System.Text;
-
+    using System.Xml;
     namespace XMLReadAndParse
     {
         public class GPXLoader
         {
-
-            static void Main(string[] args)
+            public List<List<string>> Gpx()
             {
-
                 List<List<string>> waypoints_info = new List<List<string>>();
                 var xdoc = XDocument.Load("D:\\Point de départs.gpx");
                 XNamespace ns = "http://www.topografix.com/GPX/1/1";
@@ -32,18 +30,69 @@
                     waypoints.Add(name.Lat);
                     waypoints_info.Add(waypoints);
                 }
-
-                foreach (var name in waypoints_info)
-                {
-                    foreach (String s1 in name)
-                    {
-                        Console.WriteLine(s1);
-                    }
-                    Console.WriteLine("\n");
-
-                }
-
+                return waypoints_info;
             }
+
+            public string[] BateauAutorise()
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                string TypeBateau;
+                //Lire le fichier XML
+                string test = "";
+                xmlDocument.Load("D:\\course.xml");
+                string[] tab = new string[1];
+                //Créer une liste de nœuds XML avec l'expression XPath
+                XmlNodeList xmlNodeList = xmlDocument.SelectNodes("/bats/bat");
+                foreach (XmlNode xmlNode in xmlNodeList)
+                {
+                    TypeBateau = xmlNode["typebateau"].InnerText;
+
+                    tab[0] = (xmlNode["typebateau"].InnerText);
+                    return tab;
+                }
+                return tab;
+            }
+
+            public string[] CaracteristiqueJauge()
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                string TypeBateau;
+                string LongueurCoque;
+                string LongueurHorsTout;
+                //Lire le fichier XML
+                string test = "";
+                xmlDocument.Load("D:\\imoca.xml");
+                string[] tab = new string[8];
+                //Créer une liste de nœuds XML avec l'expression XPath
+                XmlNodeList xmlNodeList = xmlDocument.SelectNodes("/bats/bat");
+
+
+
+                foreach (XmlNode xmlNode in xmlNodeList)
+                {
+                    TypeBateau = xmlNode["TypeBateau"].InnerText;
+                    LongueurCoque = xmlNode["LongueurCoque"].InnerText;
+                    LongueurHorsTout = xmlNode["LongueurHorsTout"].InnerText;
+                    string LargeurCoque = xmlNode["LargeurCoque"].InnerText;
+                    string LargeurHorsTout = xmlNode["LargeurHorsTout"].InnerText;
+                    string TirantEeau = xmlNode["TirantEeau"].InnerText;
+                    string TirantAir = xmlNode["TirantAir"].InnerText;
+                    string Poids = xmlNode["Poids"].InnerText;
+                    tab[0] = (xmlNode["TypeBateau"].InnerText);
+                    tab[1] = (xmlNode["LongueurCoque"].InnerText);
+                    tab[2] = (xmlNode["LongueurHorsTout"].InnerText);
+                    tab[3] = (LargeurCoque);
+                    tab[4] = (LargeurHorsTout);
+                    tab[5] = (TirantEeau);
+                    tab[6] = (TirantAir);
+                    tab[7] = (Poids);
+                    return tab;
+                }
+                return tab;
+            }
+
+
+
         }
     }
 }
