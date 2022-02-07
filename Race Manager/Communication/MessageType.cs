4186c28faf754dd99f6dcce63c01c4ea9c4fec6b
Sky <fabien-perot@orange.fr>
2022-02-07 13:59:26 +0100

    Message Type

---
+++ Race Manager/Communication/MessageType.cs
@@ -0,0 +1,23 @@
+﻿namespace RaceManager.Communication
+{
+    /// <summary>
+    /// Type of message send and recive.
+    /// </summary>
+    public enum IMessageType
+    {
+        CONNECTION = 0,
+        DISCONNECTION = 1,
+        PLAYERINFO = 2,
+        BOATSELECT = 3,
+        ENDRACE = 4
+    }
+
+    public enum OMessageType
+    {
+        CONNECTION = 0,
+        DISCONNECTION = 1,
+        INITRACE = 2,
+        ENVIRONMENTINFO = 3,
+        ENDRACE = 4
+    }
+}
