8cac2ef04a161d4b5927fbd3dfbf367f87a52e85
Sky <fabien-perot@orange.fr>
2022-04-06 11:17:36 +0200

    Update Polar.cs

--- RaceManager/Reading/Polar.cs
+++ RaceManager/Reading/Polar.cs
@@ -1,17 +1,17 @@
 ﻿namespace RaceManager.Reading
 {
-    public class Polar
-    {
-        //public int ID { get; set; }
-        //public string Name { get; set; }
+    //public class Polar
+    //{
+    //    //public int ID { get; set; }
+    //    //public string Name { get; set; }
 
 
-        //public List<PolFile> PolarFileLinksList = new List<PolFile>();
+    //    //public List<PolFile> PolarFileLinksList = new List<PolFile>();
 
-        public string PolName { get; set; }
-        public int PolId { get; set; }
-        public string PolPath { get; set; }
-        public string PolInformation { get; set; }
+    //    public string PolName { get; set; }
+    //    public int PolId { get; set; }
+    //    public string PolPath { get; set; }
+    //    public string PolInformation { get; set; }
 
-    }
+    //}
 }
